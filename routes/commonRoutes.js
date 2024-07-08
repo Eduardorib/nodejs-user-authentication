2ad3840e6e9901846a16b2ef0956c126cc35d82b
eduardorib <eduardoribeirogon@gmail.com>
2024-07-08 18:25:39 -0300

    feat: add Admin and Moderator routes

--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { allAccess, userBoard } = require("../controllers/userController");
+const {
+  allAccess,
+  userBoard,
+  adminBoard,
+  moderatorBoard,
+} = require("../controllers/userController");
 
 router.get("/all", allAccess); // Public Route
 router.get("/user", userBoard); // Logged User Route
+router.get("/admin", adminBoard); // Logged Admin Route
+router.get("/moderator", moderatorBoard); // Logged Moderator Route
 
 module.exports = router;
