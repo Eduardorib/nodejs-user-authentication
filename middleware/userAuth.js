a49e04a5b0d985022dd3a07f4d194c3047a7dbb9
eduardorib <eduardoribeirogon@gmail.com>
2024-07-10 17:04:59 -0300

    feat: roles check middleware

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("../models");
 
+const ROLES = db.ROLES;
 const User = db.users;
 
 // Check if username or email already exist in the database
@@ -24,6 +25,17 @@ const saveUser = async (req, res, next) => {
 
     if (emailcheck) {
       return res.status(409).json({ msg: "email already registered" });
+    }
+
+    if (req.body.roles) {
+      for (let i = 0; i < req.body.roles.length; i++) {
+        if (!ROLES.includes(req.body.roles[i])) {
+          res.status(400).json({
+            msg: "Failed! Role does not exist = " + req.body.roles[i],
+          });
+          return;
+        }
+      }
     }
 
     next();
