c58de9195b46acb7c27cf5067e63cf6db185a578
Eduardo Ribeiro <eduardoribeirogon@gmail.com>
2024-07-14 23:04:52 -0300

    feat: admin and moderator verification

--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -1,4 +1,8 @@
 const jwt = require("jsonwebtoken");
+const { createCustomError } = require("../errors/customError");
+
+const db = require("../models");
+const User = db.user;
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
@@ -6,13 +10,68 @@ const verifyToken = (req, res, next) => {
   if (!token) return next(createCustomError("token not found", 401));
 
   try {
-    const user = jwt.verify(token, process.env.secretKey);
-    req.user = user;
-    next();
+    jwt.verify(token, process.env.secretKey, (err, decoded) => {
+      req.userId = decoded.id;
+      next();
+    });
   } catch (error) {
     res.clearCookie("token");
     return next(createCustomError("user not authenticated", 403));
   }
 };
 
-module.exports = verifyToken;
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.userId);
+  const roles = await user.getRoles();
+
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "admin") {
+      next();
+      return;
+    }
+  }
+
+  res.status(403).send({
+    message: "Require Admin Role!",
+  });
+  return;
+};
+
+const isModerator = async (req, res, next) => {
+  const user = await User.findByPk(req.userId);
+  const roles = await user.getRoles();
+
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "moderator") {
+      next();
+      return;
+    }
+  }
+
+  res.status(403).send({
+    message: "Require Moderator Role!",
+  });
+};
+
+const isModeratorOrAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.userId);
+  const roles = await user.getRoles();
+
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "moderator") {
+      next();
+      return;
+    }
+
+    if (roles[i].name === "admin") {
+      next();
+      return;
+    }
+  }
+
+  res.status(403).send({
+    message: "Require Moderator or Admin Role!",
+  });
+};
+
+module.exports = { verifyToken, isAdmin, isModerator, isModeratorOrAdmin };
