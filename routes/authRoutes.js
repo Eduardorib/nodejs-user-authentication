9e246095797e0f20c3b791bb894e74c09bbf6afe
Eduardo Ribeiro <eduardoribeirogon@gmail.com>
2024-07-17 18:07:31 -0300

    feat: forgot-password route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const { signup, login } = authController;
+const { signup, login, forgotPassword } = authController;
 const userAuth = require("../middleware/userAuth");
 
 const router = express.Router();
 
 router.post("/signup", userAuth.saveUser, signup);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
 
 module.exports = router;
