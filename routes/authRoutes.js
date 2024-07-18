12c6f583e1578a6036dc0fd87ddd5cf9183e9adb
Eduardo Ribeiro <eduardoribeirogon@gmail.com>
2024-07-18 20:13:59 -0300

    feat: add reset password route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const { signup, login, forgotPassword } = authController;
+const { signup, login, forgotPassword, resetPassword } = authController;
 const userAuth = require("../middleware/userAuth");
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/signup", userAuth.saveUser, signup);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
